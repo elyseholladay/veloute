4d67a019188a4d944dc70777e0e6acdf0641df2a
Elyse Holladay Cherewaty <eholladay@rmn.com>
2016-09-19 13:26:01 -0500

    initial commit of dependencies, react, webpack, basic webpack working in CLI

---
+++ helloworld.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+  // var element = document.createElement('h1');
+  var foo = "Hello World";
+  // element.innerHTML = 'Hello world';
+
+  return foo;
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+var helloworld = require('./helloworld');
+
+// document.body.appendChild(helloworld());
+console.log(helloworld());
