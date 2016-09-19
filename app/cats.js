44ea4b6fb2436a9518094e3624eefdad61de0b2a
Elyse Holladay Cherewaty <eholladay@rmn.com>
2016-09-19 13:53:35 -0500

    🐱  😹  😻

---
+++ app/cats.js
@@ -0,0 +1,4 @@
+module.exports = function () {
+    var cats = ['dave', 'henry', 'martha'];
+    return cats;
+};
--- app/index.js
+++ app/index.js
@@ -1,4 +1,13 @@
-var helloworld = require('./helloworld');
+var cats = require('./cats');
 
-document.body.appendChild(helloworld());
-// console.log(helloworld());
+var element = document.createElement('h1');
+element.innerHTML = 'Cats';
+document.body.appendChild(element);
+
+var catDiv = document.createElement('div'); 
+document.body.appendChild(catDiv);
+
+var cats = ['dave', 'henry', 'martha'];
+for (var cat of cats) {
+    catDiv.innerHTML += cat;
+}
